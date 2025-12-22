bc729554be0ff87a030a702ad6b63eaf7676e197
AlfianndiiiDew <bagas_dewantara_34rpl@student.smktelkom-mlg.sch.id>
2025-12-22 12:53:50 +0700

    FIx panel Details

--- resources/views/orders/panel-details.blade.php
+++ resources/views/orders/panel-details.blade.php
@@ -1,4 +1,7 @@
 <x-app-layout>
+    @php
+        $serverDetails = $order->payment_details['server_details'] ?? [];
+    @endphp
     <div class="max-w-2xl mx-auto bg-white dark:bg-gray-800 p-8 rounded-lg shadow-lg my-10 text-white">
         <div class="text-center mb-8">
             <svg class="w-16 h-16 mx-auto text-green-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
